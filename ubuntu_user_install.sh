8635bdc8b5b1f7cf5533cd8c2923c4d125af6fd6
yoneyan <github.yoneyan.net>
2019-08-15 03:15:49 +0900

    Fix script

--- ubuntu_user_install.sh
+++ ubuntu_user_install.sh
@@ -2,3 +2,17 @@
 
 pip3 install setuptools
 pip3 install neovim
+
+#NVIM Setting Init
+mkdir ~/.config/nvim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+#Config File Copy
+yes | cp .tmux.conf ~/.tmux.conf
+yes | cp .zshrc ~/.zshrc
+yes | cp init.vim ~/.config/nvim/init.vim
+
+#Bash -> Zsh
+chsh -s $(which zsh)
+
