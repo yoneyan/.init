b322c44ae39a23acfaa57be7c11e2e64af967091
yoneyan <github@yoneyan.net>
2019-09-30 15:29:42 +0900

    modify install package

--- os_install/arch_install.sh
+++ os_install/arch_install.sh
@@ -4,30 +4,29 @@ yes | sudo pacman --needed -Syuu
 yes | sudo pacman --needed -S yay
 yes | sudo pacman --needed -S fcitx \
 	                      fcitx-{gtk2,gtk3,qt5,mozc,configtool} \
-                              adobe-source-han-sans-otc-fonts
-yes | sudo pacman --needed -S remmina \
-       	freerdp \
-       	libvncserver \
-       	remmina-plugin-teamviewer
-yes | sudo pacman --needed -S neovim \
-	    zsh \
-	    tmux \
-       	veracrypt \
-       	qemu \
-       	libvirt \
-       	qemu-headless \
-       	ebtables \
-       	dnsmasq \
-       	virt-install
+                              adobe-source-han-sans-otc-fonts \
+			      remmina \
+                              freerdp \
+                              libvncserver \
+			      neovim \
+	                      zsh \
+			      tmux \
+			      veracrypt \
+			      qemu \
+			      libvirt \
+			      qemu-headless \
+			      ebtables \
+			      dnsmasq \
+			      virt-install
 yes | sudo pacman --needed -S python \
-       	python-pip \
-       	clamav \
-       	powertop
+	                      python-pip \
+			      clamav \
+			      powertop
 yes | yay --needed -S docker \
-       	docker-compose \
-       	fingerprint-gui \
-	    slack-desktop \
-       	inkdrop
+	              docker-compose \
+                      fingerprint-gui \
+	              slack-desktop \
+       	              inkdrop
 yay --needed -S google-chrome \
-        virtualbox
+                virtualbox
 
