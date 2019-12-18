00141fef13b5a6a43e1e5cb080d7eaaf16d5fa0f
米田悠人 <yonedayuto1999@gmail.com>
2019-12-19 01:55:39 +0900

    windows startup script

---
+++ windows.bat
@@ -0,0 +1,12 @@
+rem choco install
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+
+choco install -y steam
+choco install -y slack
+choco -y  discord
+choco -y vnc-viewer
+choco -y teraterm wireshark veracrypt musicbee mp3tag origin
+choco -y firefox
+
+rem vscode install
+code --install-extension ms-vscode.powershell
