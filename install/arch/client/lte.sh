360b6513a108fe0a63cc401046c47c8381e0e586
yoneyan <github@yoneyan.net>
2019-12-22 10:04:33 +0900

    install required package for LTE Module

--- install/arch/client/lte.sh
+++ install/arch/client/lte.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-git clone https://github.com/mkottman/acpi_call.git
-
 git clone https://github.com/abrasive/xmm7360.git ~/git/xmm7360
+
+yes | sudo pacman -S --needed linux-latest-acpi_call 
+yes | sudo pacman -S --needed libmbim modemmanager  usbutils usb_modeswitch mobile-broadband-provider-info nm-connection-editor modem-manager-gui
