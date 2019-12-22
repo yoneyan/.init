c8a0f3d2d1747ae6b80b2d69e16c0a39f7da2b35
yoneyan <github@yoneyan.net>
2019-12-22 09:56:02 +0900

    add copy config

--- manjaro.sh
+++ manjaro.sh
@@ -19,7 +19,11 @@ done
 systemctl enable tlp
 systemctl enable tlp-sleep
 
+sudo mv ~/.config/jgmenu ~/.config/jgmenu.bak
+
 sudo cp ~/.init/config/logind.conf /etc/systemd/logind.conf
 sudo cp ~/.init/config/tlp /etc/default/tlp
 
 sudo cp ~/.init/script/lte.sh /etc/profile.d/
+sudo mv ~/.config/xfce4 ~/.config/xfce4.bak
+sudo cp -r ~/.init/config/xfce4 ~/.config
