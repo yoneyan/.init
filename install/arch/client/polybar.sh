5e51050e6730adfe6ca51438797f2e246e783666
yoneyan <github@yoneyan.net>
2020-01-06 11:51:55 +0900

    install wireless_tools

--- install/arch/client/polybar.sh
+++ install/arch/client/polybar.sh
@@ -7,3 +7,5 @@ git clone https://github.com/yoneyan/polybar-themes ~/git/polybar-themes
 cp -r ~/git/polybar-themes/polybar ~/.config/
 
 sudo cp ~/.config/polybar/fonts/* /usr/share/fonts/noto/
+
+yes | sudo pacman --needed -S wireless_tools
