bee99e9258c89fdddf4b88da99b4fed8c5e89587
yoneyan <github@yoneyan.net>
2020-01-06 12:27:30 +0900

    add install lightdm theme

--- install/manjaro/lightdm.sh
+++ install/manjaro/lightdm.sh
@@ -8,6 +8,10 @@ sudo systemctl enable lightdm
 
 git clone https://github.com/lveteau/lightdm-webkit-modern-arch-theme ~/git/lightdm-webkit-modern-arch-theme
 
+sh ~/git/lightdm-webkit-modern-arch-theme/install.sh
+sudo rm -rf ~/git/lightdm-webkit-modern-arch-theme
+
+mv /etc/lightdm/lightdm.conf /etc/lightdm/lightdm.conf.bak
 sudo cp ~/.init/config/lightdm/lightdm.conf /etc/lightdm/lightdm.conf
 sudo cp ~/.init/config/lightdm/lightdm-webkit2-greeter.conf /etc/lightdm/lightdm-webkit2-greeter.conf
 
