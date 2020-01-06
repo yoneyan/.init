cc5d6f65dfaa78f468de55c17fb8b716740fe2a7
yoneyan <github@yoneyan.net>
2020-01-06 11:53:57 +0900

    delete wwan-mineo

--- script/lte_mineo_start.sh
+++ script/lte_mineo_start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 nmcli c down wwan-mineo
+nmcli c delete wwan-mineo
+
 
 nmcli connection add con-name wwan-mineo type gsm ifname cdc-wdm0 apn mineo-d.jp user mineo@k-opti.com password mineo
 
