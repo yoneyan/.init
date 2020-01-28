aff2d7d44cfc8b0ddfaf2ba959c8c5c56b38f9e1
yoneyan <github@yoneyan.net>
2020-01-28 16:18:45 +0900

    lte mineo docomo to mineo softba

--- script/lte_mineo_start.sh
+++ script/lte_mineo_start.sh
@@ -4,7 +4,7 @@ nmcli c down wwan-mineo
 nmcli c delete wwan-mineo
 
 
-nmcli connection add con-name wwan-mineo type gsm ifname cdc-wdm0 apn mineo-d.jp user mineo@k-opti.com password mineo
+nmcli connection add con-name wwan-mineo type gsm ifname cdc-wdm0 apn mineo-s.jp user mineo@k-opti.com password mineo
 
 
 nmcli c up wwan-mineo
