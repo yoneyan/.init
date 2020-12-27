7a40f8c1d20dc86bc93b6f688e783234055f2b96
yoneyan <github@yoneyan.net>
2020-12-28 04:39:59 +0900

    tmux plugin install

--- ubuntu.sh
+++ ubuntu.sh
@@ -50,7 +50,8 @@ sudo chmod 666 /var/run/docker.sock
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-
+##tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 #zsh
 cp ~/.init/config/.zshrc ~/.zshrc
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
