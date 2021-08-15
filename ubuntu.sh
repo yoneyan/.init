ee90a17dfe41988ad58dc38e8cbe1e872e699d8f
yoneyan <github@yoneyan.net>
2021-08-15 13:52:03 +0900

    [add] Added to install vscode

--- ubuntu.sh
+++ ubuntu.sh
@@ -87,3 +87,12 @@ sudo apt install libnss3-tools && sudo dpkg -i ps-pulse-ubuntu-debian.deb
 
 # veracrypt
 sudo apt install veracrypt
+
+# vscode
+sudo apt update
+sudo apt install software-properties-common apt-transport-https wget
+cd /tmp
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt update
+sudo apt install code
